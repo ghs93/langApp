ba1a0031210c4d99bd0b9dd089826d196de590d9
hoseong <rksghtjd@gmail.com>
2021-12-09 21:21:52 +0900

    Our First Animation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Animated } from "react-native";
+import { Animated, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -7,7 +7,7 @@ const Container = styled.View`
   justify-content: center;
   align-items: center;
 `;
-const Box = styled.TouchableOpacity`
+const Box = styled.View`
   background-color: tomato;
   width: 200px;
   height: 200px;
@@ -17,16 +17,25 @@ const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
   const Y = new Animated.Value(0);
-  const moveUp = () => {};
+  const moveUp = () => {
+    Animated.spring(Y, {
+      toValue: -200,
+      bounciness: 20,
+      // friction: 1,
+      // tension: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <Container>
-      <Box
-        onPress={moveUp}
-        style={{
-          transform: [{ translateY: Y }],
-        }}
-      />
+      <TouchableOpacity onPress={moveUp}>
+        <AnimatedBox
+          style={{
+            transform: [{ translateY: Y }],
+          }}
+        />
+      </TouchableOpacity>
     </Container>
   );
 }
